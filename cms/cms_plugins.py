f5f5873a3a1ad0f19b3f845cdedd0b4732db3244
Jonas Obrist <ojiidotch@gmail.com>
2014-02-25 21:28:03 +0900

    fixed cms/cms_plugins.py

--- cms/cms_plugins.py
+++ cms/cms_plugins.py
@@ -4,6 +4,7 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import ugettext_lazy as _
 
+
 class PlaceholderPlugin(CMSPluginBase):
 
     name = _("Placeholder")
