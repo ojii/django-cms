43586121b23af717dbfd373862bf4c81ef0d08bc
Jonas Obrist <ojiidotch@gmail.com>
2014-02-25 21:54:51 +0900

    fixed cms/menu_bases.py

--- cms/menu_bases.py
+++ cms/menu_bases.py
@@ -2,11 +2,16 @@
 from menus.base import Menu
 from django.core.exceptions import ValidationError
 
+
 class CMSAttachMenu(Menu):
     cms_enabled = True
     name = None
-    
+
     def __init__(self, *args, **kwargs):
         super(CMSAttachMenu, self).__init__(*args, **kwargs)
         if self.cms_enabled and not self.name:
-            raise ValidationError("the menu %s is a CMSAttachMenu but has no name defined!" % self.__class__.__name__)
+            raise ValidationError(
+                "The menu %s is a CMSAttachMenu but has no name defined!" % (
+                    self.__class__.__name__
+                )
+            )
