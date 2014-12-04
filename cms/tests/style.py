d0e4e8943ad80b8b68737ab555a9ec4536c4bd32
Jonas Obrist <ojiidotch@gmail.com>
2014-12-05 01:46:22 +0900

    ignore migrations

--- cms/tests/style.py
+++ cms/tests/style.py
@@ -29,10 +29,12 @@ def path_to_method_name(base, path):
     return os.path.relpath(path, base).replace(os.path.sep, '__')
 
 
-def build_test_case(name, source):
+def build_test_case(name, source, ignore_paths):
     methods = {}
     base = 'test_{0}__'.format(os.path.basename(source.rstrip(os.path.sep)))
     for path in iterSourceCode([source]):
+        if path.startswith(tuple(ignore_paths)):
+            continue
         method_name = '{0}{1}'.format(base, path_to_method_name(source, path))
         methods[method_name] = test_method_factory(method_name, path)
 
@@ -40,7 +42,19 @@ def build_test_case(name, source):
 
 
 cms_path = os.path.abspath(os.path.dirname(cms.__file__))
+cms_migrations = os.path.join(cms_path, 'migrations')
+cms_django_migrations = os.path.join(cms_path, 'migrations_django')
 menus_path = os.path.abspath(os.path.dirname(menus.__file__))
+menus_migrations = os.path.join(menus_path, 'migrations')
+menus_django_migrations = os.path.join(menus_path, 'migrations_django')
 
-CMSPyflakesTests = build_test_case('CMSPyflakesTests', cms_path)
-MenusPyflakesTests = build_test_case('MenusPyflakesTests', menus_path)
+CMSPyflakesTests = build_test_case(
+    'CMSPyflakesTests',
+    cms_path,
+    [cms_migrations, cms_django_migrations]
+)
+MenusPyflakesTests = build_test_case(
+    'MenusPyflakesTests',
+    menus_path,
+    [menus_migrations, menus_django_migrations]
+)
